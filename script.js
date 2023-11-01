186614672b49d30256b61af190ccc1e0abec763e
Chandru.Dev <112074135+Chandru-SP@users.noreply.github.com>
2023-11-01 11:26:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// var joke=document.getElementById("joke");
+// var apiUrl="https://official-joke-api.appspot.com/jokes/programming/random"
+
+// fetch(apiUrl)
+//         .then((response) => response.json())
+//         .then((data) => {
+//           if (data && data.length > 0) {
+//             joke.textContent = data[0].setup + " " + data[0].punchline;
+//           } else {
+//             joke.textContent = "No jokes available.";
+//           }
+//         })
+//         .catch((error) => {
+//           joke.textContent = "Failed to fetch a joke.";
+//           console.error(error);
+//         });
